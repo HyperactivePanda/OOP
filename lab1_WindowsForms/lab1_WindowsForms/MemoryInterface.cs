d1948851a27e6eea9b1060e65daacebd99001655
Elizaveta <hyperactivepanda.2000@gmail.com>
2019-02-12 19:09:53 +0300

    WindowsForms
    Calculator for INTEGERS only

---
+++ lab1_WindowsForms/lab1_WindowsForms/MemoryInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_WindowsForms
+{
+    interface MemoryInterface
+    {
+        void Memory_Clear();
+        Double MemoryShow();
+        void M_Sum(Double b);
+        void M_Sub(Double b);
+    }
+}
