9463184317d858d2709516edd6259ea6ef80c923
Elizaveta <hyperactivepanda.2000@gmail.com>
2019-02-12 18:59:04 +0300

    WindowsForms
    Calculator

---
+++ lab1_WindowsForms/lab1_WindowsForms/CalculationMemory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_WindowsForms
+{
+    class CalculationMemory : MemoryInterface
+    {
+        private Double memory = 0;
+
+        public void Memory_Clear() //стереть содержимое регистра мамяти
+        {
+            memory = 0.0;
+        }
+        public Double MemoryShow() //показать содержимое регистра мамяти
+        {
+            return memory;
+        }
+        public void M_Sum(Double b)
+        {
+            memory += b;
+        }
+        public void M_Sub(Double b)
+        {
+            memory -= b;
+        }
+    }
+}
---
+++ lab1_WindowsForms/lab1_WindowsForms/calculator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_WindowsForms
+{
+    class calculator
+    {
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Div(int num1, int num2)
+        {
+                return num1 / num2;
+        }
+
+        public static Double Div(Double num1, Double num2) 
+        {
+            if (num2 == 0)
+                throw new System.DivideByZeroException();
+            else
+            return num1 / num2;
+        }
+
+        public static int Mult(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static Double Equally(Double num1, Double num2)
+        {
+            return num1 = num2;
+        }
+
+        public static int Div2(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+    }
+}
